fd7b3016902f287863d928d66e8ab499cd0f320f
keshavv17 <keshavsharma518@gmail.com>
2025-02-11 04:08:14 +0530

    db config and server skeleton

---
+++ config/dbConnection.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { Pool, Pool } = require('pg');
+
+const Pool = new Pool({
+    connectionString: process.env.DB_URL,
+    ssl: { rejectUnauthorized: false },
+});
+
+module.exports = {
+    query: (text, params) => Pool.query(text, params),
+    begin: () => pool.query('BEGIN'),
+    commit: () => pool.query('COMMIT'),
+    rollback: () => pool.query('ROLLBACK')
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+const authRoutes = require('./routes/auth');
+const bookingRoutes = require('./routes/booking');
+const trainRoutes = require('./routes/train');
+
+app.use('/auth',authRoutes);
+app.use('/trains',trainRoutes);
+app.use('/bookings',bookingRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
